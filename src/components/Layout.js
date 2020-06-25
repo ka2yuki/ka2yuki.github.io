f6ce535cd4ba1e62da752d0942865637e8cc2479
katsuyuki <mori1103k@gmail.com>
2020-06-25 22:15:03 +0900

    dev

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -18,7 +18,7 @@ const TemplateWrapper = ({ children }) => {
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href={`${withPrefix('/')}img/apple-touch-icon.png`}
+          href={`${withPrefix('/')}img/apple-touch-icon2.png`}
         />
         <link
           rel="icon"
