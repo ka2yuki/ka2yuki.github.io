bcc9e3e3d23a2367c2bbb34489d5d8548c4ae057
ka2yuki <mori1103k@gmail.com>
2024-02-15 02:03:31 +0900

    UPDATE: layout and comments

--- app/posts/[...slug]/page.tsx
+++ app/posts/[...slug]/page.tsx
@@ -64,8 +64,6 @@ export default async function PostPage({ params }: PostProps) {
       <a href="https://www.lancers.jp/affiliate/track?id=2154970&link=%2F" target="_blank" rel="nofollow">
         <img alt="クラウドソーシング「ランサーズ」" src="https://www.lancers.jp/img/affiliate/banner_animate468_60_1.gif" />
       </a>
-      {/* <!-- Lancers Code END -->  */}
-      {/* <!-- Lancers Code START --> */}
       <a className="" href="https://www.lancers.jp/affiliate/track?id=2154970&link=%2F" target="_blank" rel="nofollow">
         <img alt="クラウドソーシング「ランサーズ」" src="https://www.lancers.jp/img/affiliate/lancer_2_728x90.gif?v=2.0" />
       </a>
