664e65ed177b54692da1fb7ee403bbea29219176
ka2yuki <22783900+ka2yuki@users.noreply.github.com>
2025-01-30 15:31:02 +0900

    Update: fonts.css timestamp params:tada:

---
+++ js/updateTimeStamp.js
@@ -0,0 +1,7 @@
+const cssFile = "https://ka2yuki.github.io/css/fonts.css";
+const version = Date.now();
+const link = document.createElement("link");
+link.rel = "stylesheet";
+link.href = `${cssFile}?v=${version}`;
+
+document.head.appendChild(link);
