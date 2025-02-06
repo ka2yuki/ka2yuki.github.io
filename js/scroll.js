1cf857af48f0fa9a15040dd7b9c8b65f4029524a
ka2yuki <22783900+ka2yuki@users.noreply.github.com>
2025-02-06 12:06:06 +0900

    etc

---
+++ js/scroll.js
@@ -0,0 +1,23 @@
+function setFollowScroll(terget, scrollTop) {
+    console.log(scrollTop);
+    if (
+        scrollTop >= 3825 &&
+        scrollTop <= 5776
+    ) {
+        if (terget) terget.classList.remove("scrolled");
+        terget.classList.add("fixed", "fadein");
+        console.log(`target: ${terget.classList}`); // console上ではsetされているが 実際のdomに反映されない状態
+    }
+}
+function unsetFollowScroll(terget, scrollTop) {
+    console.log(scrollTop);
+    if (
+        scrollTop > 5776 && scrollTop < 6000
+    ) {
+        terget.style.zIndex = '-1';
+        terget.classList.remove("fixed", "fadein");
+        terget.classList.add("scrolled");
+    } else {
+        terget.classList.remove("fixed", "fadein");
+    }
+}
